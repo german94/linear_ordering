a53cd853780a9cd8168e9c93261eb28cca7f9bf0
German <pinzon.german.94@gmail.com>
2017-12-17 15:34:59 -0300

    comment deleted

--- final/fitness_based_selection.rb
+++ final/fitness_based_selection.rb
@@ -3,7 +3,7 @@ require './genotype'
 class FitnessBasedSelection
 
 	def select(population:)
-		population.delete(Genotype.select_worst(candidates: population))#arreglar para no tener que repetir la busqueda
+		population.delete(Genotype.select_worst(candidates: population))
 		population.delete(Genotype.select_worst(candidates: population))
 	end
 end
